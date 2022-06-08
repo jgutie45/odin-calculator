ce973e5eb8c2feb6ad4db276359889b5f3aafa1d
jasminegutierrez12 <jasmine_gutierrez12@outlook.com>
2022-06-08 18:29:13 -0500

    added Html, Css, and JavaScrip files and created addition, subtraction, multiple, and division functions

---
+++ object.js
@@ -0,0 +1,15 @@
+function addition(number1, number2){
+    return number1 + number2;
+}
+
+function subtraction(number1, number2){
+    return number1 - number2;                                                                          
+}
+
+function multiply(number1, number2){
+    return number1 * number2;
+}
+
+function divide(number1, number2){
+    return number1 / number2;
+}
