e69fcc92515adf4bfde2228ce6fc1aeaa3160b00
jasminegutierrez12 <jasmine_gutierrez12@outlook.com>
2022-06-09 11:52:16 -0500

    tested addition, subtraction, multiplication, and division with console log

--- object.js
+++ object.js
@@ -1,5 +1,5 @@
 function addition(number1, number2){
-    return number1 + number2;
+    return parseInt(number1) + parseInt(number2);
 }
 
 function subtraction(number1, number2){
@@ -13,3 +13,24 @@ function multiply(number1, number2){
 function divide(number1, number2){
     return number1 / number2;
 }
+
+const numberInput = prompt("Enter a number");
+const numberInput2 = prompt("Enter another number");
+
+additionResults = addition(numberInput, numberInput2);
+
+subtractionResults = subtraction(numberInput, numberInput2);
+
+multiplyResults = multiply(numberInput, numberInput2);
+
+divideResults = divide(numberInput, numberInput2);
+
+console.log("Addition results: " + additionResults);
+
+console.log("Subtraction results: " + subtractionResults);
+
+console.log("Multiply results: "+ multiplyResults);
+
+console.log("Division results: " + divideResults);
+
+
