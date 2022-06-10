c8ff781962e7008768aea497c65976fd78e921b0
jasminegutierrez12 <jasmine_gutierrez12@outlook.com>
2022-06-10 18:13:30 -0500

    created operate function to calculate two numbers

--- object.js
+++ object.js
@@ -14,9 +14,7 @@ function divide(number1, number2){
     return number1 / number2;
 }
 
-const numberInput = prompt("Enter a number");
-const numberInput2 = prompt("Enter another number");
-
+/*
 additionResults = addition(numberInput, numberInput2);
 
 subtractionResults = subtraction(numberInput, numberInput2);
@@ -32,5 +30,39 @@ console.log("Subtraction results: " + subtractionResults);
 console.log("Multiply results: "+ multiplyResults);
 
 console.log("Division results: " + divideResults);
+*/
 
+let numberInput = prompt("Enter a Number");
+let operator = prompt("Enter a Operator");
+let numberInput2 = prompt("Enter Another Number");
 
+function operate(operator, numberInput, numberInput2){
+    let results = 0;
+
+    switch(operator){
+        case "+":
+            results = addition(numberInput, numberInput2);
+            break;
+
+        case "-":
+            results = subtraction(numberInput, numberInput2);
+            break;
+        
+        case "*":
+            results = multiply(numberInput, numberInput2);
+            break;
+
+        case "/":
+            results = divide(numberInput, numberInput2);
+            break;
+        
+        default:
+            results = "this operator does not exist"
+    }
+
+    return results; 
+}
+
+let total = operate(operator, numberInput, numberInput2);
+
+console.log("Total: " + total);
