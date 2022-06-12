d7c3d888683890fa5e11b6cba69bfc8578e743b1
jasminegutierrez12 <jasmine_gutierrez12@outlook.com>
2022-06-11 21:34:58 -0500

    Cleaned up JS file

--- object.js
+++ object.js
@@ -14,28 +14,10 @@ function divide(number1, number2){
     return number1 / number2;
 }
 
-/*
-additionResults = addition(numberInput, numberInput2);
-
-subtractionResults = subtraction(numberInput, numberInput2);
-
-multiplyResults = multiply(numberInput, numberInput2);
-
-divideResults = divide(numberInput, numberInput2);
-
-console.log("Addition results: " + additionResults);
-
-console.log("Subtraction results: " + subtractionResults);
-
-console.log("Multiply results: "+ multiplyResults);
-
-console.log("Division results: " + divideResults);
-*/
-
-let numberInput = prompt("Enter a Number");
+/*let numberInput = prompt("Enter a Number");
 let operator = prompt("Enter a Operator");
 let numberInput2 = prompt("Enter Another Number");
-
+*/
 function operate(operator, numberInput, numberInput2){
     let results = 0;
 
@@ -63,6 +45,6 @@ function operate(operator, numberInput, numberInput2){
     return results; 
 }
 
-let total = operate(operator, numberInput, numberInput2);
+// let total = operate(operator, numberInput, numberInput2);
 
-console.log("Total: " + total);
+// console.log("Total: " + total);
