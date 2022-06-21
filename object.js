04d56ea2b8b64993ef20f95bf64bd853fec081bd
jasminegutierrez12 <jasmine_gutierrez12@outlook.com>
2022-06-20 23:43:06 -0500

    updated UI output text box Functions

--- object.js
+++ object.js
@@ -1,3 +1,26 @@
+const additionButton = document.querySelector('#additionButton');
+const subtractionButton = document.querySelector('#subtractionButton');
+const multiplyButton = document.querySelector('#multiplyButton');
+const divisionButton = document.querySelector('#divisionButton');
+const numberSevenButton = document.querySelector('#numberSeven');
+const numberSixButton = document.querySelector('#numberSix');
+const numberFiveButton = document.querySelector('#numberFive');
+const numberFourButton = document.querySelector('#numberFour');
+const numberThreeButton = document.querySelector('#numberThree');
+const numberTwoButton = document.querySelector('#numberTwo');
+const numberOneButton = document.querySelector('#numberOne');
+const numberZeroButton = document.querySelector('#numberZero');
+const outputText = document.querySelector('.outputTextBox');
+const clearButton = document.querySelector('.clearButton');
+const equalButton = document.querySelector('.equalButton');
+
+function updatingNumbers(number){
+    let currentNumber = 0;
+    outputText.textContent += number;
+    currentNumber = outputText.textContent;
+    return currentNumber;
+}
+
 function addition(number1, number2){
     return parseInt(number1) + parseInt(number2);
 }
@@ -14,11 +37,52 @@ function divide(number1, number2){
     return number1 / number2;
 }
 
-/*let numberInput = prompt("Enter a Number");
-let operator = prompt("Enter a Operator");
-let numberInput2 = prompt("Enter Another Number");
-*/
-function operate(operator, numberInput, numberInput2){
+const buttons = document.querySelectorAll('#numberButton');
+const operators = document.querySelectorAll('#operatorButton');
+
+
+let firstNumberInput = 0;
+
+let twoNumberInput = 0; 
+
+let operator = null;
+
+
+
+function firstNumber(firstNumberInput){
+    let firstNumberSelected = 0;
+    let finalFirstNumber = 0;
+    
+    buttons.forEach((button) =>{
+        button.addEventListener('click', () => {
+            firstNumberSelected = button.textContent;
+            finalFirstNumber = updatingNumbers(firstNumberSelected);
+        });
+    });
+}
+
+function operatorDisplay(){
+    operators.forEach((button) =>{
+        button.addEventListener('click', () => {
+            operator = button.textContent;
+            outputText.textContent += operator;
+            console.log(operator);
+        });
+    });
+}
+
+function clear(){
+    clearButton.addEventListener('click', function(e){
+        outputText.textContent = '';
+    });
+}
+
+firstNumber();
+operatorDisplay();
+clear();
+
+
+function operate(numberInput, operator, numberInput2){
     let results = 0;
 
     switch(operator){
@@ -44,7 +108,3 @@ function operate(operator, numberInput, numberInput2){
 
     return results; 
 }
-
-// let total = operate(operator, numberInput, numberInput2);
-
-// console.log("Total: " + total);
