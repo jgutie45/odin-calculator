b49922ffa86543f52075d2ae746e09422d738bc1
jasminegutierrez12 <jasmine_gutierrez12@outlook.com>
2022-07-26 19:26:04 -0500

    Updated Code

--- object.js
+++ object.js
@@ -1,5 +1,6 @@
 let storedFirstValue = null;
 let storedOperator = null; 
+let storedSecondValue = null;
 
 function addition(number1, number2){
     return parseInt(number1) + parseInt(number2);
@@ -42,13 +43,22 @@ function operate(number1, number2, operator){
 function start(){
     const numbers = document.querySelectorAll('.numbers');
     const operators = document.querySelectorAll('.operators');
+
+    console.log(numbers);
+    console.log(operators);
+
     const clear = document.querySelector('#clear');
     const equal = document.querySelector('#equal');
+    const uiText = document.querySelector('#text');
+    const number1 = 0;
+    const number2 = 0;
+    let equation = null;
 
     numbers.forEach((number) =>{
         number.addEventListener('click', () => {
             storedFirstValue = number.textContent;
             updateUIText(storedFirstValue);
+            console.log("Number: " + storedFirstValue);
         });
     });
 
@@ -56,17 +66,77 @@ function start(){
         operator.addEventListener('click', () => {
             storedOperator = operator.textContent;
             updateUIText(storedOperator);
+            console.log("Operator: " + storedOperator);
         });
     });
-    
+
+    clear.addEventListener('click', function(e){
+        uiText.textContent = '0';
+    });    
+
+    equal.addEventListener('click', ()=>{
+        let equationText = uiText.textContent; 
+        seperateEquation(equationText);
+    });
 }
 
 function updateUIText(text){
+    const numbers = document.querySelectorAll('.numbers');
+    const operators = document.querySelectorAll('.operators');
+    const oldText = text;
     const uiText = document.querySelector('#text');
+
+    //uiText.textContent = '';
+    /*
+        * Check which event listener is a number key or operator key
+        * Once operator has been clicked update the UI to reset for
+        the next number
+        *Then show the second number
+    */
+
     uiText.textContent = text;
+
+    /*if(numbers){
+        uiText.textContent += text;
+    }else{
+        uiText.textContent = text;
+    }
+    */
+}
+
+function seperateEquation(equationText){
+    let currentEquationText = equationText;
+    let firstNumber = 0; 
+    let secondNumber = 0;
+    let operator = null;
+    let seperateEquationText = null;
+
+    for(let i = 0; i < equationText.length; i++){
+        // Go through UI string and find the equation
+        /* seperate the numbers and account for the operators for each number*/
+        if(currentEquationText.includes("+")){
+            operator = '+';
+            return seperateEquationText = currentEquationText.split('+');
+        }else if(currentEquationText.includes("-")){
+            operator = '-';
+            return seperateEquationText = currentEquationText.split('-');
+        }else if(currentEquationText.includes("*")){
+            operator = '*';
+            return seperateEquationText = currentEquationText.split('*');
+        }else if(currentEquationText.includes("/")){
+            operator = '/';
+            return seperateEquationText = currentEquationText.split('/');
+        }
+        else{
+            outputText.textContent = 'ERROR';
+            console.log("Does Not Exist");
+        }
+    }
 }
 
 start();
+
+
 
 
 /*
